efe45d7af6154335ce88fd22615b57b02ab91786
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2022-12-25 16:51:58 +0500

    site header finished

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+
+var elBtn = document.querySelector(".js-modal");
+var elCloseBtn = document.querySelector(".modal-close");
+var elModal = document.querySelector(".modal");
+
+
+elBtn.addEventListener("click", function() {
+  elModal.classList.add("modal-open");
+  // elModal.style.display = "flex";
+});
+
+elCloseBtn.addEventListener("click", function() {
+  elModal.classList.remove("modal-open");
+});
+
+window.onclick = function(e) {
+  // console.log(e.target)
+  if(e.target == elModal){
+    elModal.classList.remove("modal-open");
+  }
+};
+
+
+// PASSWORD INPUT
+var elPassImg = document.querySelector(".eye-img");
+var elPassInput = document.querySelector(".input-password");
+
+elPassImg.addEventListener("click", function() {
+  if(elPassInput.type === "password"){
+    elPassInput.type = "text";
+    elPassImg.src = "../images/Eye-closed.svg";
+  } else {
+    elPassInput.type = "password";
+    elPassImg.src = "../images/eye-open.svg";
+  }
+});
